48969d55faeaa612669d52eb93b1d1b4385fd336
AJ Bahnken <aj.bahnken@procore.com>
2017-08-29 16:28:09 -0700

    include sslmode=require test

--- integration_test.sh
+++ integration_test.sh
@@ -6,6 +6,10 @@ psql -h 127.0.0.1 -p 5432 -U postgres
 psql -h 127.0.0.1 -p 5432 -U postgres 'sslmode=disable'
 
 echo '------------------------------------------'
+echo 'Should be getting: server does not support SSL, but SSL was required'
+psql -h 127.0.0.1 -p 5432 -U postgres 'sslmode=require'
+
+echo '------------------------------------------'
 echo 'Should be getting: "ERROR:  No such user:"'
 for x in $(seq 1 50); do
   sleep .2
